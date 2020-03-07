91ffe54e5e2a1be2ccb47de9f39c6299cc9cf50e
ehtk0502 <ehtk0502@gmail.com>
2020-03-06 19:09:37 -0800

    all configs done

---
+++ maps_app/WhereIs/App.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import MapView from 'react-native-maps';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <MapView style={{flex:0.5}}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </>
+  );
+};
+
+export default App;
